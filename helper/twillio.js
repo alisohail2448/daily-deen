9c45608adf655eaf0d35e12976891888f997d809
Sohail Akhtar Ali <sohail@truscholar.io>
2024-08-21 11:34:06 +0530

    Sohail | feat(sms): added twillio sms service

--- helper/twillio.js
+++ helper/twillio.js
@@ -1,8 +1,6 @@
-const smsId = process.env.SMS_ID;
-const smsAuthToken = process.env.SMS_AUTH_TOKEN;
+const accountSid = process.env.SMS_ID;
+const authToken = process.env.SMS_AUTH_TOKEN;
 
-const accountSid = "AC00a0e0f4c8e0f6e6bd7a872711bbe872";
-const authToken = "31fb3f75a5406f0c7aed80aa0e5efcde";
 const client = require("twilio")(accountSid, authToken);
 
 const sendSms = async (phoneNumber, password) => {
