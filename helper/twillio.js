ed0c8aafaf04bf067bc3bc0835cbdf6d1aa78ef9
Sohail Akhtar Ali <sohail@truscholar.io>
2024-08-27 11:59:09 +0530

    Sohail | feat(sms): sms sending bugs

--- helper/twillio.js
+++ helper/twillio.js
@@ -1,17 +1,20 @@
 const accountSid = process.env.SMS_ID;
 const authToken = process.env.SMS_AUTH_TOKEN;
+const phoneNumber = process.env.TWILIO_PHONE_NUMBER;
 
 console.log("accountSid", accountSid)
 console.log("authToken", authToken)
+console.log("phoneNumber", phoneNumber)
 
 const client = require("twilio")(accountSid, authToken);
 
 const sendSms = async (phoneNumber, password) => {
 //   const newPhone = "+91" + phoneNumber;
 const newPhone = "+917249047105"
+console.log("password", password)
   try {
     const response = await client.messages.create({
-      from: process.env.TWILIO_PHONE_NUMBER,
+      from: phoneNumber,
       to: newPhone,
       body: `You Added to Communtiy of Your Aalim/Hafiz. your account details are ID: ${phoneNumber}  and Password: ${password}`,
     });
